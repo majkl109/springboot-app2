54f9c99ab0cc12bcadca57267d93637ea712c379
Majkl <majkl.andracic@gmail.com>
2021-02-11 20:10:12 +0100

    Initial commit

---
+++ springboot-app2/src/test/java/com/springboot2/springbootapp2/SpringbootApp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot2.springbootapp2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApp2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
